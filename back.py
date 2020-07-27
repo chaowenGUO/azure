895de6aba0fb04e1863b3de4a2d51fdbe817336b
chaowen guo <chaowen.guo1@gmail.com>
2020-07-26 21:00:43 -0500

    Update back.py

--- back.py
+++ back.py
@@ -1,1 +1,10 @@
-
+import git, pathlib
+with git.Repo(pathlib.Path(file).resolve().parent) as repository:
+    repository.config_writer().set_value('user', 'name', 'Your Name').release()
+    repository.config_writer().set_value('user', 'email', 'you@example.com').release()
+    static = 'public'
+    repository.git.subtree('add', '--prefix=' + static, 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
+    repository.index.move([*(str(_) for _ in pathlib.Path(static).resolve().iterdir() if _.suffix == '.html' or _.suffix == '.js' or _.suffix == '.sql'), str(pathlib.Path(static).resolve().parent)], f=True)
+    repository.index.remove([static], True, r=True)
+    repository.index.commit('')#git commit --allow-empty-message -m ''
+    repository.remote().push()
