8c2cd24ad02f6898a2ba1fafe95ee3d8adb78410
chaowen guo <chaowen.guo1@gmail.com>
2020-09-02 01:49:48 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
 import sympy, js
 x= sympy.symbols('x', real=True)
 js.document.querySelector('math').textContent = sympy.printing.mathml(sympy.Integral(sympy.sqrt(1/x), x))
+#nsovinawobne'pambpambpo'eambnpemapmespabmpdfsmbp'dfmbpeamrp
