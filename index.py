29363b17e0065d3510fa010d0ef8a449a3e0577a
chaowen guo <chaowen.guo1@gmail.com>
2020-09-02 01:04:22 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 import sympy, js
 js.console.log(sympy.__version__)
 x= sympy.symbols('x', real=True)
-js.console.log(str(sympy.printing.mathml(sympy.Integral(sympy.sqrt(1/x), x))))
+#js.console.log(str(sympy.printing.mathml(sympy.Integral(sympy.sqrt(1/x), x))))
