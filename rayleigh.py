64ff930dd502376487fc4c437ad329cafbe40487
chaowen guo <chaowen.guo1@gmail.com>
2020-09-05 01:13:06 -0500

    Update rayleigh.py

--- rayleigh.py
+++ rayleigh.py
@@ -17,7 +17,7 @@ wavefunction = sympy.exp(-length * position**2)
 angularFrequency = sympy.symbols('varpi', positive=True)
 hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2) + mass * angularFrequency**2 / 2 * position**2 * wavefunction
 ground = rayleigh(hamiltonian, wavefunction, [-sympy.oo, sympy.oo])
-response += sympy.mathml(ground.subs(length, sympy.solveset(ground.diff(length), length).sup), printer='presentation'),
+response += ground.subs(length, sympy.solveset(ground.diff(length), length).sup),
 
 basis = sympy.Matrix([
     position * (length - position),
@@ -25,20 +25,20 @@ basis = sympy.Matrix([
     position * (length - position) * (length / 2 - position),
     position**2 * (length - position)**2 * (length / 2 - position)
 ])
-response += sympy.mathml(basis, printer='presentation'),
+response += basis,
 
 hamiltonian = (basis @ basis.applyfunc(lambda element: -hbar**2 / 2 / mass * element.diff(position, 2)).T).applyfunc(lambda element: sympy.integrate(element, (position, 0, length)))
-response += sympy.mathml(hamiltonian, printer='presentation'),
+response += hamiltonian,
 
 overlap = (basis @ basis.T).applyfunc(lambda element: sympy.integrate(element, (position, 0, length)))
-response += sympy.mathml(overlap, printer='presentation'),
+response += overlap,
 
 LowerInverse = overlap.cholesky().inv()
 coefficient, energy = (LowerInverse @ hamiltonian @ LowerInverse.T).diagonalize()
 coefficient = LowerInverse.T @ sympy.Matrix.hstack(*map(
     lambda column: coefficient[:, column] / coefficient[:, column].norm(),
     range(coefficient.shape[-1])))
-response += sympy.mathml(energy, printer='presentation'),
+response += energy,
 
 assert (coefficient.T @ overlap @ coefficient).applyfunc(sympy.cancel) == sympy.eye(len(basis))  
 #wavefunction are orthonormal
@@ -46,19 +46,21 @@ wavefunction = basis.T @ coefficient
 assert ((wavefunction.T @ wavefunction
         ).applyfunc(lambda element: sympy.integrate(element, (position, 0, length)).cancel()) 
         == sympy.eye(len(wavefunction)))  #wavefunction are orthonormal
-response += sympy.mathml(wavefunction.T, printer='presentation'),
+response += wavefunction.T,
 
 numerical = [(hbar, 1), (length, 1), (mass, 1)]
 import scipy.linalg
 numericalEnergy, numericalCoefficient = scipy.linalg.eigh(sympy.matrix2numpy(hamiltonian.subs(numerical), float), sympy.matrix2numpy(overlap.subs(numerical), float))
 
-response += sympy.mathml(sympy.Matrix(numericalEnergy).T, printer='presentation'),
+response += sympy.Matrix(numericalEnergy).T,
 
-response += sympy.mathml(energy.subs(numerical).evalf(), printer='presentation'),
+response += energy.subs(numerical).evalf(),
 
-response += sympy.mathml(sympy.Matrix(numericalCoefficient), printer='presentation'),
+response += sympy.Matrix(numericalCoefficient),
 
-response += sympy.mathml(coefficient.subs(numerical).evalf(), printer='presentation'),
+response += coefficient.subs(numerical).evalf(),
+
+response = [sympy.mathml(_, printer='presentation') for _ in response]
 
 import json
 with open('response.json', 'w') as _: _.write(json.dumps(response, default=str))
