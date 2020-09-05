198a6df381d06de820f392e5d125a76e473e7557
chaowen guo <chaowen.guo1@gmail.com>
2020-09-04 20:08:12 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
+def rayleigh(hamiltonian, wavefunction, limits):
+    return sympy.integrate(wavefunction * hamiltonian(wavefunction), (position, *limits)) / sympy.integrate(wavefunction**2,(position, *limits))
+
+#particle in a box
 import sympy
-x= sympy.symbols('x', real=True)
-sympy.mathml(sympy.Integral(sympy.sqrt(1/x), x), printer='presentation')
+length, mass, hbar = sympy.symbols('l,m,hbar', positive=True)
+position = sympy.symbols('x', real=True)
+wavefunction = position * (length - position)
+hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2)
+exact = hbar**2 * sympy.pi**2 / 2 / mass / length**2  #曾谨言volume1 page65 3.2.7
+sympy.N(rayleigh(hamiltonian, wavefunction, [0, length]) / exact - 1)
