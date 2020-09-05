78150d182394741374b6a4ec1236826925557d45
chaowen guo <chaowen.guo1@gmail.com>
2020-09-04 20:51:35 -0500

    Update index.py

--- index.py
+++ index.py
@@ -16,3 +16,11 @@ angularFrequency = sympy.symbols('varpi', positive=True)
 hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2) + mass * angularFrequency**2 / 2 * position**2 * wavefunction
 ground = rayleigh(hamiltonian, wavefunction, [-sympy.oo, sympy.oo])
 js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math>', sympy.mathml(ground.subs(length, sympy.solveset(ground.diff(length), length).sup), printer='presentation'), '</math>'))))
+
+basis = sympy.Matrix([
+    position * (length - position),
+    position**2 * (length - position)**2,
+    position * (length - position) * (length / 2 - position),
+    position**2 * (length - position)**2 * (length / 2 - position)
+])
+js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math>', sympy.mathml(basis, printer='presentation'), '</math>'))))
