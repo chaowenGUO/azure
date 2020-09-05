51cd14901e10d66f5ea5c3599a9f41d344a8af07
chaowen guo <chaowen.guo1@gmail.com>
2020-09-04 22:49:47 -0500

    Update rayleigh.py

--- rayleigh.py
+++ rayleigh.py
@@ -65,7 +65,7 @@ import json
 with open('out.json', 'w') as _: _.write(json.dumps(out, default=str))
 
 import git, pathlib
-with git.Repo(pathlib.Path(file).resolve().parent) as repository:
+with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
     repository.index.commit('')#git commit --allow-empty-message -m ''
