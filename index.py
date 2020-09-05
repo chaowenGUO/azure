07e6ffafb22dbd0a49191c2b6a5cb27810e1e7e3
chaowen guo <chaowen.guo1@gmail.com>
2020-09-04 20:30:55 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,10 +2,17 @@ def rayleigh(hamiltonian, wavefunction, limits):
     return sympy.integrate(wavefunction * hamiltonian(wavefunction), (position, *limits)) / sympy.integrate(wavefunction**2,(position, *limits))
 
 #particle in a box
-import sympy
+import sympy, js
 length, mass, hbar = sympy.symbols('l,m,hbar', positive=True)
 position = sympy.symbols('x', real=True)
 wavefunction = position * (length - position)
 hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2)
 exact = hbar**2 * sympy.pi**2 / 2 / mass / length**2  #曾谨言volume1 page65 3.2.7
-sympy.N(rayleigh(hamiltonian, wavefunction, [0, length]) / exact - 1)
+js.document.body.append(sympy.N(rayleigh(hamiltonian, wavefunction, [0, length]) / exact - 1))
+
+#harmonic oscillator
+wavefunction = sympy.exp(-length * position**2)
+angularFrequency = sympy.symbols('varpi', positive=True)
+hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2) + mass * angularFrequency**2 / 2 * position**2 * wavefunction
+ground = rayleigh(hamiltonian, wavefunction, [-sympy.oo, sympy.oo])
+ground.subs(length, sympy.solveset(ground.diff(length), length).sup)
