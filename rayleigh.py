8343760cdec0a9453cd33ddc00d0f602c5da46d6
chaowen guo <chaowen.guo1@gmail.com>
2020-09-04 22:36:40 -0500

    Update rayleigh.py

--- rayleigh.py
+++ rayleigh.py
@@ -1,21 +1,23 @@
+out = []
+
 def rayleigh(hamiltonian, wavefunction, limits):
     return sympy.integrate(wavefunction * hamiltonian(wavefunction), (position, *limits)) / sympy.integrate(wavefunction**2,(position, *limits))
 
 #particle in a box
-import sympy, js
+import sympy
 length, mass, hbar = sympy.symbols('l,m,hbar', positive=True)
 position = sympy.symbols('x', real=True)
 wavefunction = position * (length - position)
 hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2)
 exact = hbar**2 * sympy.pi**2 / 2 / mass / length**2  #曾谨言volume1 page65 3.2.7
-js.document.body.append(sympy.N(rayleigh(hamiltonian, wavefunction, [0, length]) / exact - 1))
+out += sympy.N(rayleigh(hamiltonian, wavefunction, [0, length]) / exact - 1),
 
 #harmonic oscillator
 wavefunction = sympy.exp(-length * position**2)
 angularFrequency = sympy.symbols('varpi', positive=True)
 hamiltonian = lambda wavefunction: -hbar**2 / 2 / mass * wavefunction.diff(position, 2) + mass * angularFrequency**2 / 2 * position**2 * wavefunction
 ground = rayleigh(hamiltonian, wavefunction, [-sympy.oo, sympy.oo])
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(ground.subs(length, sympy.solveset(ground.diff(length), length).sup), printer='presentation'), '</math>'))))
+out += sympy.mathml(ground.subs(length, sympy.solveset(ground.diff(length), length).sup), printer='presentation'),
 
 basis = sympy.Matrix([
     position * (length - position),
@@ -23,20 +25,20 @@ basis = sympy.Matrix([
     position * (length - position) * (length / 2 - position),
     position**2 * (length - position)**2 * (length / 2 - position)
 ])
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(basis, printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(basis, printer='presentation'), '</math>'))))
 
 hamiltonian = (basis @ basis.applyfunc(lambda element: -hbar**2 / 2 / mass * element.diff(position, 2)).T).applyfunc(lambda element: sympy.integrate(element, (position, 0, length)))
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(hamiltonian, printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(hamiltonian, printer='presentation'), '</math>'))))
 
 overlap = (basis @ basis.T).applyfunc(lambda element: sympy.integrate(element, (position, 0, length)))
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(overlap, printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(overlap, printer='presentation'), '</math>'))))
 
 LowerInverse = overlap.cholesky().inv()
 coefficient, energy = (LowerInverse @ hamiltonian @ LowerInverse.T).diagonalize()
 coefficient = LowerInverse.T @ sympy.Matrix.hstack(*map(
     lambda column: coefficient[:, column] / coefficient[:, column].norm(),
     range(coefficient.shape[-1])))
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(energy, printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(energy, printer='presentation'), '</math>'))))
 
 assert (coefficient.T @ overlap @ coefficient).applyfunc(sympy.cancel) == sympy.eye(len(basis))  
 #wavefunction are orthonormal
@@ -44,17 +46,20 @@ wavefunction = basis.T @ coefficient
 assert ((wavefunction.T @ wavefunction
         ).applyfunc(lambda element: sympy.integrate(element, (position, 0, length)).cancel()) 
         == sympy.eye(len(wavefunction)))  #wavefunction are orthonormal
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(wavefunction.T, printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(wavefunction.T, printer='presentation'), '</math>'))))
 
 numerical = [(hbar, 1), (length, 1), (mass, 1)]
 import scipy.linalg
 numericalEnergy, numericalCoefficient = scipy.linalg.eigh(
     sympy.matrix2numpy(hamiltonian.subs(numerical), float),
     sympy.matrix2numpy(overlap.subs(numerical), float))
-js.document.body.append(numericalEnergy)
+#js.document.body.append(numericalEnergy)
 
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(energy.subs(numerical).evalf(), printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(energy.subs(numerical).evalf(), printer='presentation'), '</math>'))))
 
-js.document.body.append(numericalCoefficient)
+#js.document.body.append(numericalCoefficient)
 
-js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(coefficient.subs(numerical).evalf(), printer='presentation'), '</math>'))))
+#js.document.body.append(js.MathJax.mathml2chtml(''.join(('<math display="block">', sympy.mathml(coefficient.subs(numerical).evalf(), printer='presentation'), '</math>'))))
+
+import json
+with open('out.json', 'w') as _: _.write(json.dumps(out))
