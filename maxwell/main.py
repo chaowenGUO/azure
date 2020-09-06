99f1c9cc992ee193ecf0041e16a683c8d27e44de
chaowen guo <chaowen.guo1@gmail.com>
2020-09-06 00:01:42 -0500

    Update main.py

--- maxwell/main.py
+++ maxwell/main.py
@@ -6,3 +6,6 @@ base=galgebra.ga.Ga('e*t|x|y|z',g=[1,-1,-1,-1],coords=sympy.symbols('t,x,y,z',re
 potential=base.mv('&phi;','vector',f=True)
 
 response += sympy.mathml(potential.obj, printer='presentation'),
+
+import json, pathlib
+with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
