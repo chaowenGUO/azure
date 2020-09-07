0692f7df112e4d23e25e8635eeed43b593515279
chaowen guo <chaowen.guo1@gmail.com>
2020-09-06 23:55:47 -0500

    Update main.py

--- maxwell/main.py
+++ maxwell/main.py
@@ -2,12 +2,12 @@ response = []
 
 import galgebra.ga, sympy
 
-base = galgebra.ga.Ga('e*t|x|y|z',g=[1,-1,-1,-1],coords=sympy.symbols('t,x,y,z',real=True))
-potential = base.mv('&phi;','vector',f=True).subs(sympy.symbols('t',real=True), sympy.symbols('t',real=True) * sympy.symbols('c',real=True))
+base = base = galgebra.ga.Ga('e*t|x|y|z',g=[sympy.symbols('c', real=True)**2,-1,-1,-1],coords=sympy.symbols('t,x,y,z',real=True))
+potential = base.mv('&phi;','vector',f=True)
 
 response += sympy.mathml(potential.obj, printer='presentation'),
 
-response += sympy.mathml((base.grad*base.grad*potential).subs(sympy.symbols('t',real=True) * sympy.symbols('c',real=True), sympy.symbols('t',real=True)).obj.doit(), printer='presentation'),
+response += sympy.mathml(base.grad*base.grad*potential, printer='presentation'),
 
 import json, pathlib
 with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
