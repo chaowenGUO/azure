e9629c0f75899811c8f58ad769bc152b7a6f4c11
chaowen guo <chaowen.guo1@gmail.com>
2020-09-06 20:33:48 -0500

    Update main.py

--- maxwell/main.py
+++ maxwell/main.py
@@ -3,11 +3,11 @@ response = []
 import galgebra.ga, sympy
 
 base=galgebra.ga.Ga('e*t|x|y|z',g=[1,-1,-1,-1],coords=sympy.symbols('t,x,y,z',real=True))
-potential=base.mv('&phi;','vector',f=True)
+potential=base.mv('&phi;','vector',f=True).subs(sympy.symbols('t',real=True), sympy.symbols('t',real=True) * sympy.symbols('c',real=True))
 
 response += sympy.mathml(potential.obj, printer='presentation'),
 
-response += sympy.mathml((base.grad*potential).obj, printer='presentation'),
+response += sympy.mathml((base.grad*base.grad*potential).obj, printer='presentation'),
 
 import json, pathlib
 with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
