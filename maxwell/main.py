de1bdef4da36938e8006d1085358e1915838bab2
chaowen guo <chaowen.guo1@gmail.com>
2020-09-06 23:04:36 -0500

    Update main.py

--- maxwell/main.py
+++ maxwell/main.py
@@ -7,7 +7,7 @@ potential = base.mv('&phi;','vector',f=True).subs(sympy.symbols('t',real=True), 
 
 response += sympy.mathml(potential.obj, printer='presentation'),
 
-response += sympy.mathml((base.grad*base.grad*potential).obj.subs(sympy.symbols('t',real=True) * sympy.symbols('c',real=True), sympy.symbols('t',real=True)).obj.doit(), printer='presentation'),
+response += sympy.mathml((base.grad*base.grad*potential).subs(sympy.symbols('t',real=True) * sympy.symbols('c',real=True), sympy.symbols('t',real=True)).obj.doit(), printer='presentation'),
 
 import json, pathlib
 with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
