2c0c6c11019c35344d6b1407acccf461eccb1a0d
chaowen guo <chaowen.guo1@gmail.com>
2020-09-13 01:38:56 -0500

    Update main.py

--- atwoodMachine/main.py
+++ atwoodMachine/main.py
@@ -4,10 +4,14 @@ import sympy
 length,mass1,mass2,gravity,time=sympy.symbols('l,m_1,m_2,g,&tstrok;',positive=True)
 x = sympy.Function('x', negative=True)(time)
 lagrangian = mass1 * x.diff(time)**2 / 2 - mass1 * gravity * x + mass2 * x.diff(time)**2 / 2 - mass2 * gravity * (length - x)
-response += sympy.mathml(sum(key * sympy.factor(value) for key, value in sympy.collect(sympy.expand(sympy.dsolve(sympy.euler_equations(lagrangian)[0]).rhs), time, evaluate=False).items()), printer='presentation'),
+solution = sum(key * sympy.factor(value) for key, value in sympy.collect(sympy.expand(sympy.dsolve(sympy.euler_equations(lagrangian)[0]).rhs), time, evaluate=False).items())
+response += solution,
 
 constraint=sympy.Function('V', real=True)(x)
 lagrangian = mass1 * x.diff(time)**2 / 2 - mass1 * gravity * x - constraint
+force = sympy.cancel(sympy.solveset(sympy.euler_equations(mass1 * x.diff(time)**2 / 2 - mass1 * gravity * x - constraint, x)[0], -constraint.diff(x)).args[0].subs(x, solution).doit())
+response += force,
 
+response = [sympy.mathml(_, printer='presentation') for _ in response]
 import json, pathlib
 with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
