f60f04b21b5c5ef99d5c53d318e68aeaccb38217
chaowen guo <chaowen.guo1@gmail.com>
2020-09-13 03:05:43 -0500

    Update main.py

--- 2.1/main.py
+++ 2.1/main.py
@@ -1,1 +1,11 @@
+response = []
 
+import sympy
+theta = sympy.symbols('theta',real=True)
+y= (sympy.symbols('l',positive=True) / 2 - sympy.symbols('a',positive=True) * sympy.csc(theta)) * sympy.cos(theta)
+motion = sympy.diff(sympy.symbols('m',positive=True) * sympy.symbols('g',positive=True) * y,theta).trigsimp()
+response += motion,
+
+response = [sympy.mathml(_, printer='presentation') for _ in response]
+import json, pathlib
+with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
