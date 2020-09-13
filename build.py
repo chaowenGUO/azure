d1679fa1eaea6c8d57a3d792b6dee64f2fadc870
chaowen guo <chaowen.guo1@gmail.com>
2020-09-13 03:53:42 -0500

    Update build.py

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import importlib
 importlib.import_module('.main', '_2_1')
 
 import git, pathlib
+print(_ for _ in pathlib.Path(__file__).resolve().parent,iterdir() if _.is_dir())
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
