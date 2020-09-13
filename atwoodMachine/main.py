7fa16d84f98aab84ce6dcb792a595746856005bc
chaowen guo <chaowen.guo1@gmail.com>
2020-09-12 23:00:20 -0500

    Update main.py

--- atwoodMachine/main.py
+++ atwoodMachine/main.py
@@ -5,3 +5,6 @@ length,mass1,mass2,gravity,time=sympy.symbols('l,m_1,m_2,g,&tstrok;',positive=Tr
 x = sympy.Function('x', real=True)(time)
 lagrangian = mass1 * x.diff(time)**2 / 2 + mass2 * x.diff(time)**2 / 2 - mass1 * gravity * x - mass2 * gravity * (length - x)
 response += sympy.euler_equations(lagrangian),
+
+import json, pathlib
+with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
