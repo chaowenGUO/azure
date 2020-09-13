f35eb3c977d43cd13b88b8b206835cd35d11dace
chaowen guo <chaowen.guo1@gmail.com>
2020-09-13 04:53:05 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 import git, pathlib, importlib
-for _ in pathlib.Path(__file__).resolve().parent.iterdir():
+parent = pathlib.Path(__file__).resolve().parent
+for _ in parent.iterdir():
     if _.is_dir() and not _.name.startswith('.'): importlib.import_module('.main', _.name)
-with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
+with git.Repo(parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
     repository.git.add(A=True)
