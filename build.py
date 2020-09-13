9ae6cca0c771534bfb9289cc21486f9b13df51c2
chaowen guo <chaowen.guo1@gmail.com>
2020-09-13 04:09:21 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,10 +1,6 @@
-import maxwell.main, rayleigh.main, atwoodMachine.main
-
-import importlib
-importlib.import_module('.main', '_2_1')
-
-import git, pathlib
-print([_ for _ in pathlib.Path(__file__).resolve().parent.iterdir() if _.is_dir() and not _.name.startswith(".")])
+import git, pathlib, importlib
+for _ in pathlib.Path(__file__).resolve().parent.iterdir():
+    if _.is_dir() and not _.name.startswith('.'): importlib.import_module('.main', _)
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
