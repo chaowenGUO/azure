6347eeefa5037cb91019a734a9d60150c0df87c4
chaowen guo <chaowen.guo1@gmail.com>
2020-09-14 01:36:13 -0500

    Update main.py

--- _2_4/main.py
+++ _2_4/main.py
@@ -11,3 +11,6 @@ y=sympy.symbols('y',real=True)
 potentialLamda=sympy.line_integrate(sympy.symbols('lamda',positive=True)*Gravity*y,curve,['x',y])
 motion=sympy.diff(potentialM+potentialm+potentialLamda,theta).trigsimp()
 response += sympy.mathml(motion, printer='presentation'),
+
+import json, pathlib
+with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
