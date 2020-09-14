ec491e0dfa83df598603859ad4560ab34330c4b0
chaowen guo <chaowen.guo1@gmail.com>
2020-09-14 06:08:36 -0500

    Update main.py

--- maxwell/main.py
+++ maxwell/main.py
@@ -2,7 +2,7 @@ response = []
 
 import galgebra.ga, sympy
 
-base = base = galgebra.ga.Ga('e*t|x|y|z',g=[sympy.symbols('c', real=True)**2,-1,-1,-1],coords=sympy.symbols('t,x,y,z',real=True))
+base = galgebra.ga.Ga('e*t|x|y|z',g=[sympy.symbols('c', real=True)**2,-1,-1,-1],coords=sympy.symbols('t,x,y,z',real=True))
 potential = base.mv('&phi;','vector',f=True)
 
 response += sympy.mathml(potential.obj, printer='presentation'),
