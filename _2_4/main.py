fe3cffde836eb4a2488932ae5c2b29963d2a4479
chaowen guo <chaowen.guo1@gmail.com>
2020-09-14 01:25:16 -0500

    Create main.py

---
+++ _2_4/main.py
@@ -0,0 +1,13 @@
+response = []
+
+import sympy
+Radius,Alpha,Gravity=sympy.symbols('a,alpha,g',positive=True)
+theta=sympy.symbols('theta',real=True)
+potentialM=sympy.symbols('M',positive=True)*Gravity*Radius*sympy.sin(sympy.pi-Alpha-theta)
+potentialm=sympy.symbols('m',positive=True)*Gravity*Radius*sympy.sin(theta-Alpha)
+phi=sympy.symbols('phi',real=True)
+curve=sympy.Curve([Radius*sympy.cos(phi),Radius*sympy.sin(phi)],(phi,theta-Alpha,theta+Alpha))
+y=sympy.symbols('y',real=True)
+potentialLamda=sympy.line_integrate(sympy.symbols('lamda',positive=True)*Gravity*y,curve,['x',y])
+motion=sympy.diff(potentialM+potentialm+potentialLamda,theta).trigsimp()
+motion
