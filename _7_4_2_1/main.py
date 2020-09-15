ef7a82f748c3f8d81e3de72a9f977e93f9505216
chaowen guo <chaowen.guo1@gmail.com>
2020-09-14 21:37:54 -0500

    Create main.py

---
+++ _7_4_2_1/main.py
@@ -0,0 +1,8 @@
+response = []
+
+import galgebra.ga, sympy
+radius, polar, azimuthal = sympy.symbols('r,theta,phi', real=True)
+cartesian = (radius * sympy.sin(polar) * sympy.cos(azimuthal), radius * sympy.sin(polar) * sympy.sin(azimuthal), radius * sympy.cos(polar))
+jacobian = sympy.Matrix(cartesian).jacobian((polar, azimuthal))
+base = galgebra.ga.Ga('\u0411',g=sympy.trigsimp(jacobian.T @ jacobian).diagonal(),coords=(polar, azimuthal))
+response += sympy.mathml(sympy.integrate((cartesian[0]**2 + cartesian[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
