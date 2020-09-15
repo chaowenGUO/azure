e79a0804e3b462aa1cd2f8ebf9ad3f0c7dd38f81
chaowen guo <chaowen.guo1@gmail.com>
2020-09-14 21:38:27 -0500

    Update main.py

--- _7_4_2_1/main.py
+++ _7_4_2_1/main.py
@@ -6,3 +6,6 @@ cartesian = (radius * sympy.sin(polar) * sympy.cos(azimuthal), radius * sympy.si
 jacobian = sympy.Matrix(cartesian).jacobian((polar, azimuthal))
 base = galgebra.ga.Ga('\u0411',g=sympy.trigsimp(jacobian.T @ jacobian).diagonal(),coords=(polar, azimuthal))
 response += sympy.mathml(sympy.integrate((cartesian[0]**2 + cartesian[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
+
+import json, pathlib
+with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
