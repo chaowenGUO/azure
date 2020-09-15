df6fca1a812528c4d3867e1679061c2ec39af0f6
chaowen guo <chaowen.guo1@gmail.com>
2020-09-15 00:24:22 -0500

    Update main.py

--- geometricAlgebra/main.py
+++ geometricAlgebra/main.py
@@ -1,3 +1,5 @@
 import galgebra.ga, sympy
 base = galgebra.ga.Ga('\u0411', coords=sympy.symbols('x:z', real=True))
 for up in range(base.n): assert sum(base.mv()[down] * base.g_inv[up, down] for down in range(base.n)) == base.mvr()[up] # base.g_inv is the inverse matrix of base.g
+for down in range(base.n): 
+    for up in range(base.n): assert (base.mv()[down] | base.mvr()[up]).simplify() == sympy.KroneckerDelta(down, up)
