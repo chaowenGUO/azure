9c6bd17511da8bfc433ba9753d9b34d4e7ec6b65
chaowen guo <chaowen.guo1@gmail.com>
2020-09-15 00:20:51 -0500

    Update main.py

--- geometricAlgebra/main.py
+++ geometricAlgebra/main.py
@@ -1,3 +1,3 @@
 import galgebra.ga, sympy
 base = galgebra.ga.Ga('\u0411', coords=sympy.symbols('x:z', real=True))
-for left in range(base.n): assert sum(base.mv()[right] * base.g_inv[left, right] for right in range(base.n)) == base.mvr()[left] # base.g_inv is the inverse matrix of base.g
+for up in range(base.n): assert sum(base.mv()[down] * base.g_inv[up, down] for down in range(base.n)) == base.mvr()[up] # base.g_inv is the inverse matrix of base.g
