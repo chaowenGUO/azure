e0e424d625c182a5f4ce6fb7194102e4e484cd09
chaowen guo <chaowen.guo1@gmail.com>
2020-09-16 03:00:54 -0500

    Update main.py

--- surfaceIntegralsOfScalarFields/main.py
+++ surfaceIntegralsOfScalarFields/main.py
@@ -5,7 +5,8 @@ radius, polar, azimuthal = sympy.symbols('r,theta,phi', real=True)
 cartesian = galgebra.ga.Ga('\u0411', g=(1,) * 3, coords=sympy.symbols('x:z', real=True))
 base = cartesian.sm((radius * sympy.sin(polar) * sympy.cos(azimuthal), radius * sympy.sin(polar) * sympy.sin(azimuthal), radius * sympy.cos(polar)), (polar, azimuthal), root='\u0411')
 #assert base.mv() == numpy.array(cartesian.mv()).T @ numpy.array(sympy.Matrix(base.u).jacobian(base.coords))
-assert base.g == sympy.trigsimp(sympy.Matrix(base.u).jacobian(base.coords).T @ sympy.Matrix(base.u).jacobian(base.coords))
+jacobian = sympy.Matrix(base.u).jacobian(base.coords)
+assert base.g == sympy.trigsimp(jacobian.T @ jacobian)
 assert base.mvr() == tuple(operator.matmul(*(numpy.array(_) for _ in (base.mv(), base.g_inv))))
 response += sympy.mathml(sympy.integrate((base.u[0]**2 + base.u[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
 
