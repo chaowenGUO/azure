4781c99494ba6c0666b851f50aff2dea4b723b83
chaowen guo <chaowen.guo1@gmail.com>
2020-09-15 23:43:45 -0500

    Update main.py

--- surfaceIntegralsOfScalarFields/main.py
+++ surfaceIntegralsOfScalarFields/main.py
@@ -4,7 +4,7 @@ import galgebra.ga, sympy
 radius, polar, azimuthal = sympy.symbols('r,theta,phi', real=True)
 cartesian = galgebra.ga.Ga('\u0411', g=(1,) * 3, coords=sympy.symbols('x:z', real=True))
 base = cartesian.sm((radius * sympy.sin(polar) * sympy.cos(azimuthal), radius * sympy.sin(polar) * sympy.sin(azimuthal), radius * sympy.cos(polar)), (polar, azimuthal), root='\u0411')
-#for _ in range(base.n): assert base.mv()[_] == sympy.Matrix(cartesian.mv()).T @ sympy.Matrix(base.u).diff(base.coords[_)
+#assert base.mv() == sympy.Matrix(cartesian.mv()).T @ sympy.Matrix(base.u).jacobian(base.coords)
 response += sympy.mathml(sympy.integrate((base.u[0]**2 + base.u[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
 
 import json, pathlib
