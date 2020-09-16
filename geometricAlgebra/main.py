ca3bc2b0081d02aab1f9ed3f0b537fc45ea89ca7
chaowen guo <chaowen.guo1@gmail.com>
2020-09-16 03:05:05 -0500

    Update main.py

--- geometricAlgebra/main.py
+++ geometricAlgebra/main.py
@@ -1,5 +1,5 @@
-import galgebra.ga, sympy
+import galgebra.ga, sympy, operator, numpy
 base = galgebra.ga.Ga('\u0411', coords=sympy.symbols('x:z', real=True))
-for up in range(base.n): assert sum(base.mv()[down] * base.g_inv[up, down] for down in range(base.n)) == base.mvr()[up] # base.g_inv is the inverse matrix of base.g
+assert base.mvr() == tuple(operator.matmul(*(numpy.array(_) for _ in (base.mv(), base.g_inv)))) # base.g_inv is the inverse matrix of base.g
 for down in range(base.n): 
     for up in range(base.n): assert (base.mv()[down] | base.mvr()[up]).simplify() == sympy.KroneckerDelta(down, up)
