8b6988ca1c02583307f11ec77a58c4e331352809
chaowen guo <chaowen.guo1@gmail.com>
2020-09-25 08:56:06 -0500

    Update main.py

--- surfaceIntegralsOfScalarFields/main.py
+++ surfaceIntegralsOfScalarFields/main.py
@@ -7,7 +7,7 @@ base = galgebra.ga.Ga('\u0411', coords=(polar, azimuthal), X=(radius * sympy.sin
 #assert base.mv() == tuple(operator.matmul(*(numpy.array(_) for _ in (cartesian.mv(), jacobian))))
 #assert base.g == sympy.trigsimp(jacobian.T @ jacobian)
 #assert base.mvr() == tuple(operator.matmul(*(numpy.array(_) for _ in (base.mv(), base.g_inv))))
-response += sympy.mathml(sympy.integrate((base.u[0]**2 + base.u[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
+response += sympy.mathml(sympy.integrate((base.X[0]**2 + base.X[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
 
 import json, pathlib
 with open(pathlib.Path(__file__).resolve().parent / 'response.json', 'w') as _: _.write(json.dumps(response))
