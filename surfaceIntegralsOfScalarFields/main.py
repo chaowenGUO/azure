92fc8bc1213f8f12bb1189a6a52188a7eae3a0aa
chaowen guo <chaowen.guo1@gmail.com>
2020-09-25 08:51:54 -0500

    Update main.py

--- surfaceIntegralsOfScalarFields/main.py
+++ surfaceIntegralsOfScalarFields/main.py
@@ -2,12 +2,11 @@ response = []
 
 import galgebra.ga, sympy, numpy, operator
 radius, polar, azimuthal = sympy.symbols('r,theta,phi', real=True)
-cartesian = galgebra.ga.Ga('\u0411', g=(1,) * 3, coords=sympy.symbols('x:z', real=True))
-base = cartesian.sm((radius * sympy.sin(polar) * sympy.cos(azimuthal), radius * sympy.sin(polar) * sympy.sin(azimuthal), radius * sympy.cos(polar)), (polar, azimuthal), root='\u0411')
-jacobian = sympy.Matrix(base.u).jacobian(base.coords)
+base = galgebra.ga.Ga('\u0411', coords=(polar, azimuthal), X=(radius * sympy.sin(polar) * sympy.cos(azimuthal), radius * sympy.sin(polar) * sympy.sin(azimuthal), radius * sympy.cos(polar)))
+#jacobian = sympy.Matrix(base.u).jacobian(base.coords)
 #assert base.mv() == tuple(operator.matmul(*(numpy.array(_) for _ in (cartesian.mv(), jacobian))))
-assert base.g == sympy.trigsimp(jacobian.T @ jacobian)
-assert base.mvr() == tuple(operator.matmul(*(numpy.array(_) for _ in (base.mv(), base.g_inv))))
+#assert base.g == sympy.trigsimp(jacobian.T @ jacobian)
+#assert base.mvr() == tuple(operator.matmul(*(numpy.array(_) for _ in (base.mv(), base.g_inv))))
 response += sympy.mathml(sympy.integrate((base.u[0]**2 + base.u[1]**2) * base.E().norm(), (polar, 0, sympy.pi / 2), (azimuthal, 0, sympy.pi / 2)), printer='presentation'),
 
 import json, pathlib
