91da2db09eac9446c3519f107714fb1d9f279cfd
chaowen guo <chaowen.guo1@gmail.com>
2020-12-22 19:05:39 -0600

    Update build.py

--- build.py
+++ build.py
@@ -1,10 +1,11 @@
-import git, pathlib, importlib
+import asyncio, pathlib, importlib
 parent = pathlib.Path(__file__).resolve().parent
 for _ in parent.iterdir():
     if _.is_dir() and not _.name.startswith('.'): importlib.import_module('.main', _.name)
-with git.Repo(parent) as repository:
-    repository.config_writer().set_value('user', 'name', 'Your Name').release()
-    repository.config_writer().set_value('user', 'email', 'you@example.com').release()
-    repository.git.add(A=True)
-    repository.index.commit('')#git commit --allow-empty-message -m ''
-    repository.remote().push()
+        
+async def f():
+    await asyncio.create_subprocess_exec('git', 'add', '-A')
+    await asyncio.create_subprocess_exec('git', 'commit', '--allow-empty-message', '-m', '')
+    await asyncio.create_subprocess_exec('git', 'push')
+    
+asyncio.run(f())
